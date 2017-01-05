716048c2539463451796b2df660626d6af119945
Alberto Jose <Alberto Jose>
2017-01-04 22:55:37 -0200

    Strong update

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,7 @@
 var i= 0;
+var color = 0;
+var color1 = "gray";
+var color2 = "white";
 setInterval(
     function() {
         i=i+0.1;
@@ -26,7 +29,27 @@ function red() {
     document.getElementById("office").style.color = "red";
     document.getElementById("office").style.transition = "all, 3.6s";
     myVar   =   setTimeout(blue, 3600);
+}
 
+function grad() {
+    document.getElementById("grad").style.background= "-moz-linear-gradient("+color+"deg, "+color1+", "+color2+")";
+    document.getElementById("grad").style.background= "-o-linear-gradient("+color+"deg, "+color1+", "+color2+")";
+    document.getElementById("grad").style.background= "background: -moz-linear-gradient("+color+"deg, "+color1+", "+color2+")";
+    document.getElementById("grad").style.background= "linear-gradient("+color+"deg, "+color1+", "+color2+")";
+    document.getElementById("grad").style.transition = "all, 3.6s";
+    color++;
+    if(color>=360){
+        color = color*(-1);
+    }
+    myVar = setTimeout(grad, 3600);
+    if(color1=="gray"){
+        color1 = "orange";
+        color2 = "gray";
+    }
+    else {
+        color1 = "gray";
+        color2 = "white";
+    }
 }
 
 red();
