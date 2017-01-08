c0832a5689a32543753d8043f1df3a175f98ad8e
Alberto Jose <Alberto Jose>
2017-01-08 01:44:30 -0200

    Final

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,6 @@
 var i= 0;
 var color = 0;
-var color1 = "gray";
-var color2 = "white";
+
 setInterval(
     function() {
         i=i+0.1;
@@ -11,45 +10,25 @@ setInterval(
         }
     },
     1);
+
 function blue() {
-    document.getElementById("name").style.color = "blue";
+    document.getElementById("name").style.color = "#00f";
     document.getElementById("name").style.transition = "all, 3.6s";
-    document.getElementById("divisor").style.borderColor = "blue";
+    document.getElementById("divisor").style.borderColor = "#00f";
     document.getElementById("divisor").style.transition = "all, 3.6s";
-    document.getElementById("office").style.color = "blue";
+    document.getElementById("office").style.color = "#00f";
     document.getElementById("office").style.transition = "all, 3.6s";
     myVar   =   setTimeout(red, 3600);
 }
 
 function red() {
-    document.getElementById("name").style.color = "red";
+    document.getElementById("name").style.color = "#f00";
     document.getElementById("name").style.transition = "all, 3.6s";
-    document.getElementById("divisor").style.borderColor = "red";
+    document.getElementById("divisor").style.borderColor = "#f00";
     document.getElementById("divisor").style.transition = "all, 3.6s";
-    document.getElementById("office").style.color = "red";
+    document.getElementById("office").style.color = "#f00";
     document.getElementById("office").style.transition = "all, 3.6s";
     myVar   =   setTimeout(blue, 3600);
-}
-
-function grad() {
-    document.getElementById("grad").style.background= "-moz-linear-gradient("+color+"deg, "+color1+", "+color2+")";
-    document.getElementById("grad").style.background= "-o-linear-gradient("+color+"deg, "+color1+", "+color2+")";
-    document.getElementById("grad").style.background= "background: -moz-linear-gradient("+color+"deg, "+color1+", "+color2+")";
-    document.getElementById("grad").style.background= "linear-gradient("+color+"deg, "+color1+", "+color2+")";
-    document.getElementById("grad").style.transition = "all, 3.6s";
-    color++;
-    if(color>=360){
-        color = color*(-1);
-    }
-    myVar = setTimeout(grad, 3600);
-    if(color1=="gray"){
-        color1 = "orange";
-        color2 = "gray";
-    }
-    else {
-        color1 = "gray";
-        color2 = "white";
-    }
 }
 
 red();
